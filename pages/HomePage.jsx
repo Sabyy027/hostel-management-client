a8855f4aa92d000578ffb757fa76f76b4da95613
Sabyy027 <sabeerreigns@gmail.com>
2025-11-14 20:27:40 +0530

    Initial setup: Vite, React, and Tailwind v4

---
+++ pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hostel Management System - Client
+      </h1>
+      <p className="ml-2 text-xl">(Vite + React + Tailwind)</p>
+    </div>
+  );
+};
+
+export default HomePage;
