89fdad149d6c25ca111286c90444179c89354b69
Sabyy027 <sabeerreigns@gmail.com>
2025-11-24 23:34:55 +0530

    issue in login fix

--- src/components/StaffLayout.jsx
+++ src/components/StaffLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import NotificationBell from './NotificationBell';
 import StaffMobileMenu from './StaffMobileMenu';
--- src/components/StudentLayout.jsx
+++ src/components/StudentLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import StudentSidebar from './StudentSidebar';
 import NotificationBell from './NotificationBell';
 import ProfileDropdown from './ProfileDropdown';
